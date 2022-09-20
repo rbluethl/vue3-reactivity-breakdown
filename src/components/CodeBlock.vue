999b156e145dda5e451d7e10d1547473f4cc2814
Ronald Blüthl <ronald@devoted.dev>
2022-09-20 12:55:53 +0200

    Reduced code block padding

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <pre class="rounded !bg-neutral-800 !text-sm">
+  <pre class="rounded !bg-neutral-800 !py-0 !text-sm">
     <code class="language-markup !font-mono !bg-neutral-800">{{script}}</code>
     <code class="language-markup !font-mono !bg-neutral-800 ">{{template}}</code>
   </pre>
