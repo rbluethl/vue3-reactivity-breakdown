1b9cefccf2549869c15c5c9686b63c65a87a6470
Ronald Blüthl <ronald@devoted.dev>
2022-09-20 11:13:17 +0200

    Added meta description, added more padding to Links

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="inline-flex items-center space-x-1 font-medium text-emerald-500 hover:text-emerald-600"
+    class="inline-flex items-center space-x-1 py-0.5 font-medium text-emerald-500 hover:text-emerald-600"
     :href="href"
     target="_blank"
   >
